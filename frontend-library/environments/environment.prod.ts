8e827a3e64c9e5aa3d659175eb04bec384148124
Vaibhav L <loharvb@gmail.com>
2026-01-13 18:53:55 +0530

    environments created..

---
+++ frontend-library/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:8080/api',
+    appName: 'frontend-library',
+    version: '1.0.0'
+};
---
+++ frontend-library/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: process.env['NG_APP_API_URL'] || 'http://localhost:8080/api',
+    appName: 'frontend-library',
+    version: '1.0.0'
+};
